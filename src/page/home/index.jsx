3a8ae840b6f22af38d100a446d78116c4bbea088
Salimzoda <119676998+Salimz0da@users.noreply.github.com>
2023-06-20 08:56:00 +0500

    Add files via upload

--- src/page/home/index.jsx
+++ src/page/home/index.jsx
@@ -18,6 +18,17 @@ import clearBox from "../../img/section/material/glass-box-clear.png";
 import jutePlastmase from "../../img/section/material/Jute-placemat.png";
 import whiteBasket from "../../img/section/material/basket-white.png";
 import basketStandard from "../../img/section/material/standard-basket.png";
+import star from "../../img/section/svg-resurs/star.svg";
+
+import withFood from '../../img/section/material-2/withFood.png'
+import twill from '../../img/section/material-2/twill-seat.png'
+import fluter from '../../img/section/material-2/fluted-beverage.png'
+import beverageGlass from '../../img/section/material-2/ceramix-egg.png'
+import bombobox from '../../img/section/material-2/Bambobox.png'
+import metallCutlery from '../../img/section/material-2/metallCutlery.png'
+import ceramicCups from '../../img/section/material-2/4packCeramixCups.png'
+import lineNapkins from '../../img/section/material-2/lineNaplins.png'
+import stoneware from '../../img/section/material-2/stonewre.png'
 
 export default function Home() {
   return (
@@ -92,11 +103,282 @@ export default function Home() {
               <div className="card-imageholder">
                 <img src={whiteVase} alt="error" />
               </div>
-
               <div className="card-content">
-                <h3>Далеко-далеко за словесными горами.</h3>
+                <h3 className="card-title-2">DECOR</h3>
+                <p className="text-card-3">
+                  Lorem ipsum dolor sit amet, <br /> consectetur adipiscing
+                  elit, <br /> sed do eiusmod tempor <br /> incididunt ut labore
+                  et dolore <br /> magna aliqua.
+                </p>
+                <a href="" className="viewNow viewNow-whitw">
+                  VIEW ALL
+                </a>
+              </div>
+              <div className="text-all-3">
+                <p className="text-card-2">DECOR</p>
+                <img src={star} alt="error" />
+                <p className="card-price">30$</p>
               </div>
             </div>
+            {/* Card 1 */}
+            <div className="card card-mini">
+              <div className="card-imageholder">
+                <img src={towel} alt="error" />
+              </div>
+              <div className="card-content card-content-mini">
+                <h3 className="card-title-2">LINEN BEACH TOWEL</h3>
+                <p className="text-card-3">
+                  Lorem ipsum dolor sit amet, <br /> consectetur adipiscing
+                  elit, <br /> sed do eiusmod tempor <br /> incididunt ut labore
+                  et dolore <br /> magna aliqua.
+                </p>
+                <a href="" className="viewNow viewNow-whitw">
+                  VIEW ALL
+                </a>
+              </div>
+              <div className="text-all-3 text-all-3-mini">
+                <p className="text-card-2">LINEN BEACH TOWEL</p>
+                <img src={star} alt="error" />
+                <p className="card-price">30$</p>
+              </div>
+            </div>
+            {/* Card2 */}
+            <div className="card card-mini">
+              <div className="card-imageholder">
+                <img src={glassBox} alt="error" />
+              </div>
+              <div className="card-content ">
+                <h3 className="card-title-2">Square Clear Glass Box</h3>
+                <p className="text-card-3">
+                  Lorem ipsum dolor sit amet, <br /> consectetur adipiscing
+                  elit, <br /> sed do eiusmod tempor <br /> incididunt ut labore
+                  et dolore <br /> magna aliqua.
+                </p>
+                <a href="" className="viewNow viewNow-whitw">
+                  VIEW ALL
+                </a>
+              </div>
+              <div className="text-all-3 text-all-3-mini">
+                <p className="text-card-2">Square Clear Glass Box</p>
+                <img src={star} alt="error" />
+                <p className="card-price">30$</p>
+              </div>
+            </div>
+            {/* Crad3 */}
+            <div className="card card-mini">
+              <div className="card-imageholder">
+                <img src={ceramixPlates} alt="error" />
+              </div>
+              <div className="card-content">
+                <h3 className="card-title-2">4-pack Small Ceramic Plates</h3>
+                <p className="text-card-3">
+                  Lorem ipsum dolor sit amet, <br /> consectetur adipiscing
+                  elit, <br /> sed do eiusmod tempor <br /> incididunt ut labore
+                  et dolore <br /> magna aliqua.
+                </p>
+                <a href="" className="viewNow viewNow-whitw">
+                  VIEW ALL
+                </a>
+              </div>
+              <div className="text-all-3">
+                <p className="text-card-2">4-pack Small Ceramic Plates</p>
+                <img src={star} alt="error" />
+                <p className="card-price">30$</p>
+              </div>
+            </div>
+            {/* Card4 */}
+            <div className="card card-mini">
+              <div className="card-imageholder">
+                <img src={clearBox} alt="error" />
+              </div>
+              <div className="card-content">
+                <h3 className="card-title-2">Large Clear Glass Box</h3>
+                <p className="text-card-3">
+                  Lorem ipsum dolor sit amet, <br /> consectetur adipiscing
+                  elit, <br /> sed do eiusmod tempor <br /> incididunt ut labore
+                  et dolore <br /> magna aliqua.
+                </p>
+                <a href="" className="viewNow viewNow-whitw">
+                  VIEW ALL
+                </a>
+              </div>
+              <div className="text-all-3">
+                <p className="text-card-2">Large Clear Glass Box</p>
+                <img src={star} alt="error" />
+                <p className="card-price">30$</p>
+              </div>
+            </div>
+            {/* Card5 */}
+            <div className="card card-mini">
+              <div className="card-imageholder">
+                <img src={jutePlastmase} alt="error" />
+              </div>
+              <div className="card-content">
+                <h3 className="card-title-2">Round Jute Placemat</h3>
+                <p className="text-card-3">
+                  Lorem ipsum dolor sit amet, <br /> consectetur adipiscing
+                  elit, <br /> sed do eiusmod tempor <br /> incididunt ut labore
+                  et dolore <br /> magna aliqua.
+                </p>
+                <a href="" className="viewNow viewNow-whitw">
+                  VIEW ALL
+                </a>
+              </div>
+              <div className="text-all-3">
+                <p className="text-card-2">Round Jute Placemat</p>
+                <img src={star} alt="error" />
+                <p className="card-price">30$</p>
+              </div>
+            </div>
+            {/* Card6 */}
+            <div className="card card-mini">
+              <div className="card-imageholder">
+                <img src={whiteBasket} alt="error" />
+              </div>
+              <div className="card-content">
+                <h3 className="card-title-2">Metal Wire Basket</h3>
+                <p className="text-card-3">
+                  Lorem ipsum dolor sit amet, <br /> consectetur adipiscing
+                  elit, <br /> sed do eiusmod tempor <br /> incididunt ut labore
+                  et dolore <br /> magna aliqua.
+                </p>
+                <a href="" className="viewNow viewNow-whitw">
+                  VIEW ALL
+                </a>
+              </div>
+              <div className="text-all-3">
+                <p className="text-card-2">Metal Wire Basket</p>
+                <img src={star} alt="error" />
+                <p className="card-price">30$</p>
+              </div>
+            </div>
+            {/* Card7 */}
+            <div className="card card-mini">
+              <div className="card-imageholder">
+                <img src={basketStandard} alt="error" />
+              </div>
+              <div className="card-content">
+                <h3 className="card-title-2">BOHO CHIC</h3>
+                <p className="text-card-3">
+                  Lorem ipsum dolor sit amet, <br /> consectetur adipiscing
+                  elit, <br /> sed do eiusmod tempor <br /> incididunt ut labore
+                  et dolore <br /> magna aliqua.
+                </p>
+                <a href="" className="viewNow viewNow-whitw">
+                  VIEW ALL
+                </a>
+              </div>
+              <div className="text-all-3">
+                <p className="text-card-2">BOHO CHIC</p>
+                <img src={star} alt="error" />
+                <p className="card-price">30$</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="add-badsheed">
+          <div className="add-badsheed-card">
+            <h4 className="add-badsheed-card-title">BEDSHEET SETS</h4>
+            <p className="add-badsheed-card-price">
+              $50.00 <span>$220.00</span>
+            </p>
+            <p className="add-badsheed-card-text">
+              Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit,
+              sed do eiusmod tempor.
+            </p>
+            <button className="viewNow">VIEW DETAILS</button>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="container">
+          <h2 className="head-title">TOP RATING</h2>
+          <div className="card-material-row">
+            <div className="card-material">
+              <img src={withFood} alt="error" />
+              <div className="card-info-material">
+                <p>Tray with Foot</p>
+                <img src={star} alt="error" />
+                <h5>30$</h5>
+              </div>
+            </div>
+            {/* Card */}
+            <div className="card-material">
+              <img src={twill} alt="error" />
+              <div className="card-info-material">
+                <p>Fluted Beverage Glass</p>
+                <img src={star} alt="error" />
+                <h5>30$</h5>
+              </div>
+            </div>
+            {/* Card */}
+             <div className="card-material">
+              <img src={fluter} alt="error" />
+              <div className="card-info-material">
+                <p>Glazed Stoneware Plate</p>
+                <img src={star} alt="error" />
+                <h5>30$</h5>
+              </div>
+            </div>
+            {/* Card */}
+            <div className="card-material">
+              <img src={beverageGlass} alt="error" />
+              <div className="card-info-material">
+                <p>Ceramic Egg Cup</p>
+                <img src={star} alt="error" />
+                <h5>30$</h5>
+              </div>
+            </div>
+            {/* Card */}
+            <div className="card-material">
+              <img src={bombobox} alt="error" />
+              <div className="card-info-material">
+                <p>Small Bamboo Box</p>
+                <img src={star} alt="error" />
+                <h5>30$</h5>
+              </div>
+            </div>
+            {/* Card */}
+            <div className="card-material">
+              <img src={metallCutlery} alt="error" />
+              <div className="card-info-material">
+                <p>Metal Cutlery Basket</p>
+                <img src={star} alt="error" />
+                <h5>30$</h5>
+              </div>
+            </div>
+            {/* Card */}
+            <div className="card-material">
+              <img src={ceramicCups} alt="error" />
+              <div className="card-info-material">
+                <p>Twill Seat Cushion</p>
+                <img src={star} alt="error" />
+                <h5>30$</h5>
+              </div>
+            </div>
+            <div className="card-material">
+              <img src={lineNapkins} alt="error" />
+              <div className="card-info-material">
+                <p>Twill Seat Cushion</p>
+                <img src={star} alt="error" />
+                <h5>30$</h5>
+              </div>
+            </div>
+            {/* Crad */}
+            {/* Crad */}
+            <div className="card-material">
+              <img src={stoneware} alt="error" />
+              <div className="card-info-material">
+                <p>4-pack Ceramic Cups</p>
+                <img src={star} alt="error" />
+                <h5>30$</h5>
+              </div>
+            </div>
+            {/* Card */}
+           
+            
           </div>
         </div>
       </section>
